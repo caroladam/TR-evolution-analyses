cfa849eb143e0c947fd89cb739d62b6f79e437d5
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-05-29 14:22:14 -0700

    Create run_trf.sh

---
+++ run_trf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+genomesdir=$1
+trfdir=$2
+
+if [ -z $genomesdir ]
+then
+	echo 'Script requires path to FASTA files as argument'
+fi
+
+if [ -z $trfdir ]
+then
+	echo 'Script requires path to TRF executable as argument'
+fi
+
+echo "Running TRF"
+
+# trf config
+matchscore=2
+mismatchscore=5
+indelscore=7
+pm=80
+pi=10
+minscore=24 
+maxperiod=2000 
+
+# run trf 
+for file in $genomesdir/*.fa
+do
+  $trfdir/trf $file ${matchscore} ${mismatchscore} ${indelscore} ${pm} ${pi} ${minscore} ${maxperiod} -f -d -h -l 6
+done
