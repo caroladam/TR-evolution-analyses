c44d273bc59e47990e65c4a186f752be48333b91
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-07-01 13:40:01 -0700

    Update align_lifted_TRs.sh

--- align_lifted_TRs.sh
+++ align_lifted_TRs.sh
@@ -4,36 +4,48 @@
 # author: Adam, Carolina de Lima (2024)
 
 usage() {
-  echo "Usage: $0 -a <lifted_trs> -b <query_tr_list> -c <target_prefix> -d <query_prefix>"
-  exit 1
+    echo "Usage: $0 -l <lifted_trs> -c <query_tr_catalog> -t <target_prefix> -q <query_prefix> -o <overlp_perc> -a <align_perc>"
+    exit 1
 }
 
 # Parse command-line options
-while getopts ":a:b:c:d:" opt
+while getopts ":l:c:t:q:o:a" opt
 do
-  case ${opt} in
-    a) lifted_trs="$OPTARG" ;;
-    b) query_tr_list="$OPTARG" ;;
-    c) target_prefix="$OPTARG" ;;
-    d) query_prefix="$OPTARG" ;;
-    *) usage ;;
-  esac
+    case ${opt} in
+        l) lifted_trs="$OPTARG" ;;
+        c) query_tr_catalog="$OPTARG" ;;
+        t) target_prefix="$OPTARG" ;;
+        q) query_prefix="$OPTARG" ;;
+        o) overlap_perc="$OPTARG" ;;
+        a) align_perc="$OPTARG" ;;
+        *) usage ;;
+    esac
 done
 
 # Check if all required arguments are provided
-if [ -z "$lifted_trs" ] || [ -z "$query_tr_list" ] || [ -z "$target_prefix" ] || [ -z "$query_prefix" ]
-then
-  usage
+if [ -z "$lifted_trs" ] || [ -z "$query_tr_catalog" ] || [ -z "$target_prefix" ] || [ -z "$query_prefix" ] || [ -z "$overlap_perc" ] || [ -z "$align_perc" ]
+ then
+    usage
 fi
+
+# Ensure required commands are available
+for cmd in bedtools awk needle parallel grep paste sed
+do
+    if ! command -v "$cmd" &> /dev/null
+then
+        echo "Error: $cmd is not installed or not in PATH."
+        exit 1
+    fi
+done
 
 # Create directories for fasta files and aligned files
 fasta_dir=$(mktemp -d fasta_XXXXXX)
 aligned_dir="aligned"
 mkdir -p "$aligned_dir"
 
-# Intersect regions of the lifted TRs file with TRF catalog for the query genome - keeping those with >=50% reciprocal overlap
+# Intersect regions of the lifted TRs file with TRF catalog for the query genome - keeping those with threshold reciprocal overlap
 shared_trs="shared_trs.bed"
-bedtools intersect -a "$lifted_trs" -b "$query_tr_list" -wa -wb -f 0.50 -r > "$shared_trs"
+bedtools intersect -a "$lifted_trs" -b "$query_tr_catalog" -wa -wb -f "$overlap_perc" -r > "$shared_trs"
 
 # Add species names and keep only columns of interest - index and motif sequence, then split file to keep one TR per file
 awk -v tgt="$target_prefix" -v qry="$query_prefix" \
@@ -43,7 +55,7 @@ awk -v tgt="$target_prefix" -v qry="$query_prefix" \
 cd "$fasta_dir"
 for file in x*
 do
-  awk 'BEGIN {FS="\t"; OFS="_"} {print ">"$1, $2, $3, $4"\n"$5"\n"">"$6, $7, $8, $9, $11, $12, $13, $14"\n"$10}' "$file" > "$file.fa"
+    awk 'BEGIN {FS="\t"; OFS="_"} {print ">"$1, $2, $3, $4"\n"$5"\n"">"$6, $7, $8, $9, $11, $12, $13, $14"\n"$10}' "$file" > "$file.fa"
 done
 
 # Align fasta files with Needle using GNU Parallel
@@ -52,24 +64,29 @@ parallel -j $(nproc) "needle -asequence {1} -bsequence {1} -gapopen 10 -gapexten
 cd ../
 
 # Get alignment similarity scores
+score_file="score.txt"
+bedloc_file="bedloc.txt"
+output_file="sim_score_${target_prefix}_${query_prefix}"
+
 for file in "${aligned_dir}"/*.out
 do
-  # Extract similarity scores
-  grep "Similarity" "${file}" | grep -o "[0-9]*\.[0-9]*" >> score.txt
-  grep ">${query_prefix}" "${file}" >> bedloc.txt
+    # Extract similarity scores
+    grep "Similarity" "${file}" | grep -o "[0-9]*\.[0-9]*" >> "$score_file"
+    grep ">${query_prefix}" "${file}" >> "$bedloc_file"
 done
 
 # Combine bed locations and scores into a single file with target and query prefixes
-paste bedloc.txt score.txt > "sim_score_${target_prefix}_${query_prefix}"
-rm bedloc.txt score.txt
+paste "$bedloc_file" "$score_file" > "$output_file"
+rm "$bedloc_file" "$score_file"
 
 # Clean up unnecessary text from the output file
-sed -i 's/[a-z]*\.fa\.out:>//g' "sim_score_${target_prefix}_${query_prefix}"
+sed -i 's/[a-z]*\.fa\.out:>//g' "$output_file"
 
+# Get subset of TRs with threshold alignment similarity
+awk -v align_perc="$align_perc" '$2 >= align_perc' "$output_file" | sed 's/_/\t/g; s/"//g'
+awk 'BEGIN {FS="\t"; OFS="\t"} {print $2, $3, $4, $1, $5, $6, $7, $8}' "$output_file" > "sim_score_${overlap_perc}_${align_perc}_${target_prefix}_${query_prefix}.bed"
+
+# Clean up temporary files
 rm -r "$fasta_dir"
-
-# Get subset of TRs with >=50% alignment similarity
-awk '$2 >= 50' sim_score_${target_prefix}_${query_prefix} | sed 's/_/\t/g; s/"//g'
-awk 'BEGIN {FS="\t"; OFS="\t"} {print $2, $3, $4, $1, $5, $6, $7, $8}' sim_score_${target_prefix}_${query_prefix} > sim_score_50_50_${target_prefix}_${query_prefix}.bed
 
 echo "Alignment similarity score extraction and formatting completed."
